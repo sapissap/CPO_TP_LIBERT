2cc16c2a08963350b58900fa0972301ccfd346e1
pauline <pauline@192.168.1.92>
2022-11-15 17:11:10 +0100

    Creation des classes dans le bon endroit

---
+++ TP3_LIBERT/src/tp3_libert/TP3_LIBERT.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_libert;
+import Armes.*;
+import Personnages.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pauline
+ */
+public class TP3_LIBERT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String name,int Att) {
+        Baton Baton1 =new Baton();
+            Baton.nom
+            
+       
+       
+        // TODO code application logic here
+    }
+
+}
